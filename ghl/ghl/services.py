637a163c4641c2d3ffde0c8a271bd61452209419
iDanorever <danolex654321@gmail.com>
2025-10-20 13:42:31 -0500

    Delete ghl/ghl directory

--- ghl/ghl/services.py
+++
@@ -1,7 +0,0 @@
-def add_contact_tag(contact_id, tag_name):
-    # Lógica para añadir una etiqueta a un contacto
-    pass
-
-def update_contact_custom_field(contact_id, field, value):
-    # Lógica para actualizar un campo custom de un contacto
-    pass
